989208668953a800a2c27cc6dc9c6076520cce6f
Shantnyu <144965598+Shantnyu@users.noreply.github.com>
2023-09-17 12:22:42 -0400

    webServer.py

--- webServer.py
+++ webServer.py
@@ -1,4 +1,4 @@
-# Import socket module
+# import socket module
 from socket import *
 # In order to terminate the program
 import sys
@@ -27,54 +27,35 @@ def webServer(port=13331):
             # Open the client requested file in binary mode
             f = open(filename[1:], "rb")
 
-            # Get the file content
-            file_content = f.read()
-            f.close()
-
-            # Determine the content type based on the file extension
-            content_type = b"text/html; charset=UTF-8"  # Default to HTML
-
-            if filename.endswith(b".jpg"):
-                content_type = b"image/jpeg"
-            elif filename.endswith(b".png"):
-                content_type = b"image/png"
-
-            # Headers for a valid HTTP response
-            response_headers = [
-                b"HTTP/1.1 200 OK\r\n",
-                b"Server: SimpleWebServer\r\n",
-                b"Content-Type: " + content_type + b"\r\n",
-                b"Content-Length: " + bytes(len(file_content)) + b"\r\n",
-                b"Connection: close\r\n",
-                b"\r\n"
-            ]
-
-            # Send the response headers
-            for header in response_headers:
-                connectionSocket.send(header)
+            # Headers for validating HTTP request
+            connectionSocket.send(b"HTTP/1.1 200 OK\r\n")
+            connectionSocket.send(b"Content-Type: text/html; charset=UTF-8\r\n")
+            connectionSocket.send(b"Server: SimpleWebServer\r\n")
+            connectionSocket.send(b"Connection: close\r\n")
+            # Send blank line to end headers
+            connectionSocket.send(b"\r\n")
 
             # Send the content of the requested file to the client
-            connectionSocket.send(file_content)
+            for i in f:
+                connectionSocket.send(i)
+            f.close()
 
-            # Close the connection socket
+            # Closing the connection socket
             connectionSocket.close()
 
-        except FileNotFoundError:
-            # Handle file not found errors with a 404 response
-            error_response = (
-                b"HTTP/1.1 404 Not Found\r\n"
-                b"Content-Type: text/html; charset=UTF-8\r\n"
-                b"Server: SimpleWebServer\r\n"
-                b"Connection: close\r\n"
-                b"\r\n"
-                b"<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n"
-            )
-            connectionSocket.send(error_response)
+        except Exception:
+            # Headers for handling bad requests
+            connectionSocket.send(b"HTTP/1.1 404 Not Found\r\n")
+            connectionSocket.send(b"Content-Type: text/html; charset=UTF-8\r\n")
+            connectionSocket.send(b"Server: SimpleWebServer\r\n")
+            connectionSocket.send(b"Connection: close\r\n")
+            connectionSocket.send(b"\r\n")
+            connectionSocket.send(b"<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n")
             connectionSocket.close()
-        except Exception as e:
-            # Handle other exceptions
-            print(f"An error occurred: {e}")
-            connectionSocket.close()
+
+    # Commenting out the below, as it's technically not required and some students have moved it erroneously in the While loop. DO NOT DO THAT OR YOU'RE GONNA HAVE A BAD TIME.
+    # serverSocket.close()
+    # sys.exit()  # Terminate the program after sending the corresponding data
 
 if __name__ == "__main__":
     webServer(13331)
