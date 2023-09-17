77be8f31f07e50a9939785f1c0b664f2900f3c8c
Shantnyu <144965598+Shantnyu@users.noreply.github.com>
2023-09-17 12:17:04 -0400

    webServer.py

--- webServer.py
+++ webServer.py
@@ -9,7 +9,7 @@ def webServer(port=13331):
     # Prepare a server socket
     serverSocket.bind(("127.0.0.1", port))
 
-    # Start listing on port
+    # Start listening on port
     serverSocket.listen(1)
 
     while True:
@@ -30,6 +30,8 @@ def webServer(port=13331):
             # Headers for validating http request
             connectionSocket.send(b"HTTP/1.1 200 OK\r\n")
             connectionSocket.send(b"Content-Type: text/html; charset=UTF-8\r\n")
+            connectionSocket.send(b"Server: SimpleWebServer\r\n")
+            connectionSocket.send(b"Connection: close\r\n")
             # send blank line to end headers
             connectionSocket.send(b"\r\n")
 
@@ -37,25 +39,27 @@ def webServer(port=13331):
             for i in f:
                 connectionSocket.send(i)
             f.close()
-            connectionSocket.send(b"HTTP/1.1 404 Not Found\r\n")
-            connectionSocket.send(b"Content-Type: text/html; charset=UTF-8\r\n")
-            connectionSocket.send(b"Server: SimpleWebServer\r\n")
-            # send blank line to end headers
-            connectionSocket.send(b"\r\n")
-            connectionSocket.send(b"<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n")
+
             # closing the connection socket
             connectionSocket.close()
 
-        except Exception:
-            # Headers for handeling bad requests
+        except FileNotFoundError:
+            # Handle file not found errors
             connectionSocket.send(b"HTTP/1.1 404 Not Found\r\n")
             connectionSocket.send(b"Content-Type: text/html; charset=UTF-8\r\n")
+            connectionSocket.send(b"Server: SimpleWebServer\r\n")
+            connectionSocket.send(b"Connection: close\r\n")
             connectionSocket.send(b"\r\n")
             connectionSocket.send(b"<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n")
             connectionSocket.close()
-    # Commenting out the below, as its technically not required and some students have moved it erroneously in the While loop. DO NOT DO THAT OR YOURE GONNA HAVE A BAD TIME.
-    serverSocket.close()
-    sys.exit()  # Terminate the program after sending the corresponding data
+        except Exception as e:
+            # Handle other exceptions
+            print(f"An error occurred: {e}")
+            connectionSocket.close()
+
+    # Commenting out the below, as it's technically not required and some students have moved it erroneously in the While loop. DO NOT DO THAT OR YOU'RE GONNA HAVE A BAD TIME.
+    # serverSocket.close()
+    # sys.exit()  # Terminate the program after sending the corresponding data
 
 
 if __name__ == "__main__":
