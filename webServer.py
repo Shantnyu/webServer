a6b4e5cbdba5b8265e587ef3407ce768394b07c4
Shantnyu <144965598+Shantnyu@users.noreply.github.com>
2023-09-17 12:00:04 -0400

    webServer.py

--- webServer.py
+++ webServer.py
@@ -36,6 +36,10 @@ def webServer(port=13331):
             # Send the content of the requested file to the client
             for i in f:
                 connectionSocket.send(i)
+            f.close()
+            connectionSocket.send(b"Content-Type: text/html; charset=UTF-8\r\n")
+            connectionSocket.send(b"Server: SimpleWebServer\r\n")
+            connectionSocket.send(b"Connection: close\r\n\r\n")    
             # closing the connection socket
             connectionSocket.close()
 
