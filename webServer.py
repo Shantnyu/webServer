8e430979435d116bb1dc6da50f684a27f66a974b
Shantnyu <144965598+Shantnyu@users.noreply.github.com>
2023-09-18 18:23:06 -0400

    webServer.py

--- webServer.py
+++ webServer.py
@@ -1,3 +1,4 @@
+
 # import socket module
 from socket import *
 # In order to terminate the program
@@ -9,49 +10,49 @@ def webServer(port=13331):
     # Prepare a server socket
     serverSocket.bind(("127.0.0.1", port))
 
-    # Start listening on port
+    # Start listing on port
     serverSocket.listen(1)
 
     while True:
         # Establish the connection
         print("Ready to serve...")
 
-        # Accept incoming connection
+        # accept incoming connection
         connectionSocket, addr = serverSocket.accept()
 
         try:
-            # Handle messages received, sent by the client
+            # handle messages received, sent by client
             message = connectionSocket.recv(1024).decode()
             filename = message.split()[1]
 
-            # Open the client requested file in binary mode
+            # opens the client requested file in binary mode
             f = open(filename[1:], "rb")
 
-            # Send the HTTP response headers
+            # Headers for validating http request
             connectionSocket.send(b"HTTP/1.1 200 OK\r\n")
-            connectionSocket.send(b"Server: SimpleWebServer\r\n")
-            connectionSocket.send(b"Connection: close\r\n")
             connectionSocket.send(b"Content-Type: text/html; charset=UTF-8\r\n")
-            # Send blank line to end headers
+            # send blank line to end headers
             connectionSocket.send(b"\r\n")
 
             # Send the content of the requested file to the client
             for i in f:
                 connectionSocket.send(i)
             f.close()
-
-            # Closing the connection socket
+            connectionSocket.send(b"Content-Type: text/html; charset=UTF-8\r\n")
+            # closing the connection socket
             connectionSocket.close()
 
         except Exception:
-            # Headers for handling bad requests
+            # Headers for handeling bad requests
             connectionSocket.send(b"HTTP/1.1 404 Not Found\r\n")
-            connectionSocket.send(b"Server: SimpleWebServer\r\n")
-            connectionSocket.send(b"Connection: close\r\n")
             connectionSocket.send(b"Content-Type: text/html; charset=UTF-8\r\n")
             connectionSocket.send(b"\r\n")
             connectionSocket.send(b"<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n")
             connectionSocket.close()
+    # Commenting out the below, as its technically not required and some students have moved it erroneously in the While loop. DO NOT DO THAT OR YOURE GONNA HAVE A BAD TIME.
+    serverSocket.close()
+    sys.exit()  # Terminate the program after sending the corresponding data
+
 
 if __name__ == "__main__":
     webServer(13331)
